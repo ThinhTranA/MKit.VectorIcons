ca8b13836534f44b4dc1f3e31ea488251e029949
Thinh Tran <thinhtran@outlook.com.au>
2021-09-26 19:29:19 +0800

    add missing xaml.cs

---
+++ example/Maui.VectorIconsSample/Maui.VectorIconsSample/Maui.VectorIconsSample/IconButtonPage.xaml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Maui.VectorIconsSample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class IconButtonPage : ContentPage
+    {
+        public IconButtonPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
